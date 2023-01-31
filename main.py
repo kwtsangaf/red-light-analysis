de25fbe46e7d4ef7dca78990dd9d377d33ab7f19
raytsangdev <raytsangdev@gmail.com>
2023-01-31 15:49:10 -0500

    feat: sort the graph

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import plotly.graph_objects as go
 import plotly.io as pio
 
 pio.templates.default = "plotly_white"
-
 data = pd.read_csv("data2.csv")
+
+data = data.set_index(['Charges Laid by Location & Year'])['2020'].nlargest(10).reset_index() # get top 10 row
+# data = data.sort_values('2020', ascending=False)  # sort all rows by column '2020'
 
 figure = px.bar(data,
                 x='Charges Laid by Location & Year',
